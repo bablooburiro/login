c8470ac19c4ad156c538428aaebeb27f877e9ac8
bablooburiro <bablooburiro6@gmail.com>
2025-05-02 11:26:17 +0500

    Ye mera dusra comment hai

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Hello extends Component {
     render() {
-        return <h1>I am class component</h1>
+        return <h1>I am class component How are you</h1>
     }
 }
 export default Hello;
